8d5d8ecde81ae5f90fea9513b167bbf62573383e
taylord65 <taylor.dotsikas@mail.mcgill.ca>
2014-05-12 18:25:45 -0400

    Parse and validation working together

--- app/models/video.rb
+++ app/models/video.rb
@@ -8,16 +8,19 @@ class Video < ActiveRecord::Base
 #NEED TO VALIDATE youtube url  
   validates :url, :format => {
       :with    => %r{https://www.youtube.com/}i,
-      :message => 'must be a youtube video.' }
-      
+      :message => 'must be a https youtube video.' }
       
       def converturl(url)
+        if url.include? "https://www.youtube.com/" 
         #converts a url into a video id
+
         regex = /(?:.be\/|\/watch\?v=|\/(?=p\/))([\w\/\-]+)/
         video_id = url.match(regex)[1]
         video_id
-
       end
+      
+      end
+    
   
   
 end
