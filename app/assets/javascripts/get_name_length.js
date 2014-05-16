cc532a63e1874a9f2789b43c35a22999a401ad97
taylord65 <taylor.dotsikas@mail.mcgill.ca>
2014-05-16 10:10:52 -0400

    removed extra js file

--- app/assets/javascripts/get_name_length.js
+++
@@ -1,7 +0,0 @@
-<script type="text/javascript">
-    function youtubeFeedCallback(json){
-    document.write("Title: " + json["data"]["title"] + "<br>");
-    document.write("Duration: " + json["data"]["duration"] + "<br>");
-    }
-</script>
-<script type="text/javascript" src=("http://gdata.youtube.com/feeds/api/videos/" + W8YAK8oMEKI + "?v=2&alt=jsonc&callback=youtubeFeedCallback&prettyprint=true")></script>
