d1caf9b7673dc4710086ba8682d5ab3e03898d5c
taylord65 <taylor.dotsikas@mail.mcgill.ca>
2014-05-19 11:14:24 -0400

    Live Stream fixed loop issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140517145056) do
+ActiveRecord::Schema.define(version: 20140519151120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140517145056) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "reprogrammed_at"
   end
 
   create_table "videos", force: true do |t|
