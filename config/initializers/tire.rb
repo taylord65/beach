2c0028d13da525463110b9a9fe2f17d0da874b29
taylord65 <taylor.dotsikas@mail.mcgill.ca>
2014-05-21 14:19:52 -0400

    search error fix 2

--- config/initializers/tire.rb
+++ config/initializers/tire.rb
@@ -1,5 +1,3 @@
 Tire.configure do
-  url "http://localhost:9200"
-  #you can uncomment the next line if you want to see the elasticsearch queries in their own seperate log
-  #logger "#{Rails.root}/log/es.log" 
+  url "http://pzsragkr:ji8ohgwec6n0uhni@cherry-8351282.us-east-1.bonsai.io"
 end
