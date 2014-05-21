3b5948acd08a387870d9978ac538bd28a9cbac90
taylord65 <taylor.dotsikas@mail.mcgill.ca>
2014-05-21 13:18:09 -0400

    bonsai url set elasticsearch

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -10,7 +10,6 @@ class Stream < ActiveRecord::Base
   include Tire::Model::Search
   include Tire::Model::Callbacks
   
-
   def self.search(params)
     tire.search(load: true) do
       query { string params[:query], default_operator: "AND" } if params[:query].present?
