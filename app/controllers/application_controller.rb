644871f340dd3999b22aea5fcbc7583e6bf6e346
taylord65 <taylor.dotsikas@mail.mcgill.ca>
2014-12-20 11:36:14 -0500

    test commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   
   before_filter :set_trending
   def set_trending
-    @trending1 = "blackkeys"
+    @trending1 = "smashbros"
      @trending2 = "vice"
       @trending3 = "latenight"
   end
