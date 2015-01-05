c312a4e94205f9fe6d31ed566e0b65bd17959a66
taylord65 <taylor.dotsikas@mail.mcgill.ca>
2015-01-04 21:27:33 -0500

    forgot asset compile

--- config/initializers/tire.rb
+++ config/initializers/tire.rb
@@ -2,4 +2,7 @@ Tire.configure do
  url "http://pzsragkr:ji8ohgwec6n0uhni@cherry-8351282.us-east-1.bonsai.io"
 # url "http://localhost:9200"
    logger 'log/elasticsearch.log' 
+   
+#   RAILS_ENV=production bundle exec rake assets:precompile
+
 end
